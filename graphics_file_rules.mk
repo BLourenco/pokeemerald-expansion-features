24edac0e588558507949131c20c008f77c895afc
BLourenco <brandon.lourenco@gmail.com>
2023-04-08 16:13:23 -0400

    Generate and import type symbols.

--- graphics_file_rules.mk
+++ graphics_file_rules.mk
@@ -422,6 +422,9 @@ graphics/party_menu/bg.4bpp: %.4bpp: %.png
 $(TYPESGFXDIR)/move_types.4bpp: $(types:%=$(TYPESGFXDIR)/%.4bpp) $(contest_types:%=$(TYPESGFXDIR)/contest_%.4bpp)
 	@cat $^ >$@
 
+$(TYPESGFXDIR)/move_types_icons.4bpp: $(types:%=$(TYPESGFXDIR)/%_icon.4bpp)
+	@cat $^ >$@
+
 $(TYPESGFXDIR)/move_types.gbapal: $(TYPESGFXDIR)/move_types_1.gbapal \
                                   $(TYPESGFXDIR)/move_types_2.gbapal
 	@cat $^ >$@
